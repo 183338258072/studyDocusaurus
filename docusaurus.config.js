71d1c8aba163fc9243bfb4a525561d04241ccdcf
李富鹏 <v18338258072@163.com>
2024-01-25 17:31:55 +0800

    20240125

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'lookmori',
+  title: '随心的前端学习日记',
   tagline: 'Dinosaurs are cool',
   staticDirectories: ['public', 'static'],
   url: 'https://your-docusaurus-test-site.com',
